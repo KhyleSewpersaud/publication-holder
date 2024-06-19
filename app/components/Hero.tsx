0d024313032ace80abea248731605cefe5d6d2a2
KhyleSewpersaud <khylesewpersaud@gmail.com>
2024-06-19 13:53:54 -0400

    Created routes to different pages

---
+++ app/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <header>
+      <div>
+        <h1>Represent Your Research</h1>
+        <p>Easily integrate your Google Scholar publications to your website</p>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
---
+++ app/components/Nav.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import Temp from "../../public/temp.png";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className="flex justify-between py-16 px-32">
+      <div>
+        <Image src={Temp} width={50} alt="Logo" />
+      </div>
+      <div className="flex justify-center font-bold text-lg">
+        <div className="flex w-full mr-24">
+          <Link href="/" className="mx-5">Home</Link>
+          <Link href="/demo" className="mx-5">Demo</Link>
+          <Link href="/about" className="mx-5">About</Link>
+          <Link href="/pricing" className="mx-5">Pricing</Link>
+        </div>
+
+        <div className="font-bold text-lg">
+          <Link href="/dashboard">Dashboard</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
