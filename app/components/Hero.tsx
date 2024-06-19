af6d6e2c00a479899ee4fe2a90dce42d914727af
KhyleSewpersaud <khylesewpersaud@gmail.com>
2024-06-19 14:48:33 -0400

    Hero roughly done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import HeroImage from "../../public/Frame 198.png"
 
 const Hero = () => {
   return (
-    <header>
-      <div>
-        <h1>Represent Your Research</h1>
-        <p>Easily integrate your Google Scholar publications to your website</p>
+    <header className="flex justify-between">
+      <div className="flex flex-col w-1/2">
+        <h1 className="font-black text-7xl mb-5">Represent Your Research</h1>
+        <p className="my-5 text-lg">Easily integrate your Google Scholar publications to your website</p>
+        <Link href="/demo" className="btn btn-neutral w-1/4 my-5">See How</Link>
+      </div>
+      <div className="flex justify-center w-1/2">
+        <Image src={HeroImage} width={350} alt="Sample Publication Widget" />
       </div>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
 
-const dm_sans = DM_Sans({ subsets: ["latin"]});
+const dm_sans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "PubView",
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={dm_sans.className}>
+      <body data-theme="cmyk" className={dm_sans.className}>
         <Nav />
         {children}
       </body>
